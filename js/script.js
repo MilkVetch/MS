95263c49c401ac1255c24f333adedf7a92f98e4f
milkvetch <457804000@qq.com>
2016-02-18 01:23:38 +1300

    1

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$('.btn-box').click(function() {
+	$('.menu-btn').toggleClass('active');
+	$('.h-con').toggleClass('active');
+});
+
+$('.h-item').click(function() {
+	$('.menu-btn').toggleClass('active');
+	$('.h-con').toggleClass('active');
+});
